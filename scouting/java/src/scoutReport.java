bc780d9780bf29f4cfa6c8777bf6542f6dfb8ca4
Cody <codymb36@gmail.com>
2015-01-22 13:58:39 -0600

    Started state machine for scoutReport.java

--- scouting/java/src/scoutReport.java
+++ scouting/java/src/scoutReport.java
@@ -43,6 +43,8 @@ class team {
         System.out.println("Kickstands: " + autoKickstands + "/" + numOfMatches);
         System.out.println("Center goals: " + autoCenterGoals + "/" + numOfMatches);
         System.out.println("Rolling goals: " + autoRollingGoals + "/" + numOfMatches);
+        System.out.println("~ Team Score ~");
+        System.out.println(this.getScore());
     }
 
     public int getScore () {
@@ -55,18 +57,102 @@ class team {
 }
 
 public class scoutReport {
+
     public static void main (String[] args) {
-        team _4150 = new team("4150");
-        team _4324 = new team("4324");
-        int[][] scoutData = {{4150,1,0,0,1},{4150,1,0,0,1},{4324,0,0,0,1},{4324,1,0,0,1}};
-        for (int i = 0; i < scoutData.length; i++) {
-            int[] matchStats = Arrays.copyOfRange(scoutData[i], 1, scoutData[i].length);
-            if (scoutData[i][0] == 4150) {
-                _4150.addMatch(matchStats);
+        System.out.println("Program Started");
+        // team _4150 = new team("4150");
+        // team _4324 = new team("4324");
+        // int[][] scoutData = {{4150,1,0,0,1},{4150,1,0,0,1},{4324,0,0,0,1},{4324,1,0,0,1}};
+        // for (int i = 0; i < scoutData.length; i++) {
+        //     int[] matchStats = Arrays.copyOfRange(scoutData[i], 1, scoutData[i].length);
+        //     if (scoutData[i][0] == 4150) {
+        //         _4150.addMatch(matchStats);
+        //     } else if (scoutData[i][0] == 4324) {
+        //         _4324.addMatch(matchStats);
+        //     }
+        // }
+
+        /*Start CLI state machine*/
+        System.out.println("Checkpoint 1");
+        String queue = "";
+        String nextState = "INIT";
+
+        while (nextState != "quit") {
+            switch (nextState) {
+
+                case "INIT":
+                    System.out.println("The init case ran");
+                    /*Add teams to a list*/
+                    String teamNumbers = "4150,4324" ;
+                    String[] teamList = teamNumbers.split(",");
+                    System.out.println("Teams Present");
+                    for (int i = 0; i < teamList.length; i++) {
+                        System.out.println(teamList[i]);
+                    }
+                    queue += "IDLE*";
+                    break;
+
+                case "IDLE":
+                    System.out.println("Please enter command.");
+                    String command = "rankings list";
+                    if (command == "rankings list") {
+                        System.out.println("Printing a list of rankings");
+                        queue += "FUNCTION2*";
+                    } else if (command == "exit") {
+                        queue += "EXIT*";
+                    } else {
+                        System.out.println("Command not recognized.");
+                    }
+
+                    break;
+
+                /*SOURCE OF ERROR*/
+                case "getRankings":
+                    System.out.println("The rankings case ran");
+                    queue += "IDLE*";
+                    break;
+
+                case "FUNCTION2":
+                    System.out.println("The funciton2 case has ran");
+                    break;
+
+                case "EXIT":
+                    System.out.println("The program will exit");
+                    break;
+
+                default:
+                    queue += "EXIT*";
+                    break;
             }
+
+            if (queue.indexOf("*") != -1) {
+                /*
+                Takes the next string from the que (from the begining of
+                the string to the asterisk)
+                */
+                nextState = queue.substring(0, queue.indexOf("*"));
+                /*Removes the next state from the queue*/
+                queue = queue.substring((nextState.length() + 1), queue.length());
+            } else {
+                nextState = "quit";
+            }
+
         }
-        _4150.getStats();
-        _4324.getStats();
-        System.out.println("\n4150's score: " + _4150.getScore());
+
     }
+
+    // public static void getRankings (int numOfRanks) {
+    //     String[] fakeScores = {"474", "415"};
+    //     String[] teamRanks = sortTeams(fakeScores);
+    //
+    //     for (int i = 1; i <= numOfRanks; i++) {
+    //         System.out.println(i + ") " + teamRanks[i-1]);
+    //     }
+    //
+    // }
+    //
+    // public static String[] sortTeams (String[] teamScores) {
+    //     String[] sorted = {"4150", "4324"};
+    //     return sorted;
+    // }
 }
