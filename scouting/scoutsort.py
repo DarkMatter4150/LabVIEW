fd8946a0c731966e61334ca1edc11854e6b9939e
Cody <cmbrooks@users.noreply.github.com>
2015-01-28 08:16:28 -0600

    Implemented QSM

--- scouting/scoutsort.py
+++ scouting/scoutsort.py
@@ -7,12 +7,33 @@ import operator
 filedata = "1234,9,8\n4324,6,27\n967,0,92\n"
 
 a = filedata.splitlines()
-#print a
 b = []
 row = 0
 for row in a:
      b.append(row.rsplit(","))
-#print b
-c =[]
-c = sorted(b, key=operator.itemgetter(2,1))
-print c
+b = sorted(b, key=operator.itemgetter(2,1))
+print b
+
+#Start QSM
+queue = "*"
+nextState = "INIT"
+quit = False
+while (quit != True):
+    if nextState == "INIT":
+        print "INIT case ran"
+        queue += "IDLE*"
+    elif nextState == "IDLE":
+        print "IDLE case ran"
+        queue += "EXIT*"
+    elif nextState == "EXIT":
+        print "EXIT case ran"
+        queue = ""
+        
+    #Load next queue
+    try:
+        delimeterIndex = queue.index("*")
+        nextState = queue[0:delimeterIndex]
+        queue = queue[delimeterIndex+1:len(queue)]
+    except ValueError:
+        print "Queue is empty"
+        quit = True
